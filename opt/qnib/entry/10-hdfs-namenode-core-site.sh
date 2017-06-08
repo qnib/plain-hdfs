36a93822945a07050a2874e15b2de6b446552827
Christian Kniep <christian@qnib.org>
2017-06-08 17:01:53 +0200

    moved config to parent plain-hadoop

--- opt/qnib/entry/10-hdfs-namenode-core-site.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-
-cat /opt/qnib/hdfs/etc/core-site.xml \
- > /opt/hadoop/etc/hadoop/core-site.xml
--- opt/qnib/entry/11-hdfs-namenode-hdfs-site.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-
-cat /opt/qnib/hdfs/namenode/etc/hdfs-site.xml \
- > /opt/hadoop/etc/hadoop/hdfs-site.xml
